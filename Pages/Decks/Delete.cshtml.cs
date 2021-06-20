0f0a1f6acdc1034b1321392efdbae0ed82397e35
Your Name <noahluvsmilkduds@gmail.com>
2021-06-19 23:03:02 -0500

    fixes for deckerbuilder commit deck deletion

--- Pages/Decks/Delete.cshtml.cs
+++ Pages/Decks/Delete.cshtml.cs
@@ -1,3 +1,5 @@
+using System.Linq;
+
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -49,11 +51,32 @@ namespace MTGViewer.Pages.Decks
 
         public async Task<IActionResult> OnPostAsync(int id)
         {
-            Deck = await _context.Locations.FindAsync(id);
+            Deck = await _context.Locations
+                .Include(l => l.Cards)
+                .ThenInclude(ca => ca.Card)
+                .SingleOrDefaultAsync(l => l.Id == id);
 
             if (Deck != null)
             {
+                var availCards = Deck.Cards
+                    .Select(ca => ca.Card.Id)
+                    .Distinct()
+                    .ToArray();
+
+                var availables = await _context.Cards
+                    .Where(c => availCards.Contains(c.Id))
+                    .Select(c => c.Amounts.Single(ca => ca.Location == null))
+                    .ToDictionaryAsync(ca => ca.Card.Id);
+
+                foreach(var ca in Deck.Cards)
+                {
+                    availables[ca.Card.Id].Amount += ca.Amount;
+                }
+
+                _context.RemoveRange(Deck.Cards);
+
                 _context.Locations.Remove(Deck);
+
                 await _context.SaveChangesAsync();
             }
 
