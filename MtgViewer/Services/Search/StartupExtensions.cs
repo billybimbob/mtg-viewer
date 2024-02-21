3d7042ded93eab9eaa5dfb8b69fa14ef5c2463a4
billybimbob <noahluvsmilkduds@gmail.com>
2024-02-20 21:46:15 -0600

    register db

--- MtgViewer/Services/Search/StartupExtensions.cs
+++ MtgViewer/Services/Search/StartupExtensions.cs
@@ -1,20 +1,40 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
 using MtgApiManager.Lib.Service;
 
 using MtgViewer.Services.Search;
+using MtgViewer.Services.Search.Database;
 
 namespace Microsoft.Extensions.DependencyInjection;
 
 public static partial class StartupExtensions
 {
-    public static IServiceCollection AddMtgQueries(this IServiceCollection services)
+    public static IServiceCollection AddMtgQueries(this IServiceCollection services, IConfiguration config)
     {
-        services
-            .AddSingleton<IMtgServiceProvider, MtgServiceProvider>()
-            .AddScoped(provider => provider
-                .GetRequiredService<IMtgServiceProvider>()
-                .GetCardService());
+        var options = new PrintingOptions();
 
-        return services
-            .AddScoped<IMtgQuery, MtgApiQuery>();
+        config.GetSection(nameof(PrintingOptions)).Bind(options);
+
+        if (options.UseLocal && options.FilePath is not null)
+        {
+            services.AddDbContext<AllPrintingsDbContext>(optionsBuilder =>
+            {
+                optionsBuilder.UseSqlite(options.FilePath);
+            });
+        }
+        else
+        {
+            services
+                .AddSingleton<IMtgServiceProvider, MtgServiceProvider>()
+                .AddScoped(provider => provider
+                    .GetRequiredService<IMtgServiceProvider>()
+                    .GetCardService());
+
+            services
+                .AddScoped<IMtgQuery, MtgApiQuery>();
+        }
+
+        return services;
     }
 }
